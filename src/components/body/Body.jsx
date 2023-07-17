d0c18f2382209ddbbe8fa3a95ace528251296895
Ankit <2002ankit2002@gmail.com>
2023-07-17 16:22:33 +0530

    commiting structure

---
+++ src/components/body/Body.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import bg from "./img/bg.png";
+import "./body.css";
+// import Navbar from "../nav/Navbar";
+
+function Body() {
+  return (
+    <>
+      <div className="background">
+        <img src={bg} alt="bgimg" className="image" />
+      </div>
+
+      <div class="n">
+        <div class="inner_div"></div>
+        <div class="inner_2nd_div"></div>
+      </div>
+    </>
+  );
+}
+
+export default Body;
---
+++ src/components/nav/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./nav.css";
+// import logo from "../../assets/logo.png";
+import logo from "../../assets/logo.png";
+
+function Navbar() {
+  return (
+    <div className="navbar flex">
+      <div className="start_nav flex">
+        <img src={logo} />
+        <h5>Buy in Dwarka Delhi</h5>
+      </div>
+      <div className="middle_nav">
+        <ul className="flex">
+          <li>For Buyers</li>
+          <li>For Buyers</li>
+          <li>For Buyers</li>
+          <li>For Buyers</li>
+          <li>For Buyers</li>
+        </ul>
+      </div>
+      <div className="navbar__whitediv">Post Property</div>
+    </div>
+  );
+}
+
+export default Navbar;
